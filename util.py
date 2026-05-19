8cc4eb7f7b6db95524f271e960d88d02c59ffeeb
Dillon Beresford <retromanc3r@outlook.com>
2026-05-18 23:22:09 -0500

    test vuln against codeql

---
+++ util.py
@@ -0,0 +1,12 @@
+# The following code is vulnerable to command injections because 
+# it is using untrusted inputs to set up a new process. Therefore 
+# an attacker can execute an arbitrary program that is installed 
+# on the system.
+
+from flask import request
+import os
+
+def ping():
+    cmd = "ping -c 1 %s" % request.args.get("host", "www.google.com")
+    status = os.system(cmd) # Noncompliant
+    return str(status == 0)
